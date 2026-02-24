a4a53db50f25e4192e24ce614e69a55e50f6baf3
Nitin Rana <nitinrana2519@gmail.com>
2026-02-24 22:15:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-let listSection = document.getElementById('band')
+let listSection = document.getElementById('bands')
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 let ignore = new Set(['A', 'An','The'])
 
